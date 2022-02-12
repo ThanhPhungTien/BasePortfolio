2f677d32f56117d2d5ed3cfddf817ea8e610c940
= <phungtienthanh135@gmail.com>
2022-02-12 19:02:33 +0700

    update new design

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: themeData,
+      title: 'Phùng Tiến Thành',
       debugShowCheckedModeBanner: false,
       initialRoute: RouteNames.home,
       onGenerateRoute: (RouteSettings settings) => routeSettings(settings),
